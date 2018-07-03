b8f44f26d747de189f5c90a08fd6f60c7f8ee5f2
Zsemle <zsemlerider@gmail.com>
2018-07-03 14:15:59 +0200

    weekends included

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ var CalculateDueDate = require('./due-date-calculator');
 
 let currentDate = new Date(Date.now())
 let submitDate = currentDate;
-let turnaroundTime = 16;
+let turnaroundTime = 32;
 
 console.log('Expected Resolution by: ', CalculateDueDate(submitDate,turnaroundTime));
