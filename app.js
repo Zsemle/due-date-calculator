7dd094643bfe88c680d8b6d0291322d5d5cef48d
Zsemle <zsemlerider@gmail.com>
2018-07-03 13:41:19 +0200

    First version to work with some errors

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 var CalculateDueDate = require('./due-date-calculator');
 
-console.log(CalculateDueDate());
+let currentDate = new Date(Date.now())
+let submitDate = currentDate;
+let turnaroundTime = 16;
+
+console.log('Expected Resolution by: ', CalculateDueDate(submitDate,turnaroundTime));
--- due-date-calculator.js
+++ due-date-calculator.js
@@ -1,5 +1,30 @@
 function CalculateDueDate(submitDate,turnaroundTime){
-  let resolvedBy = Date.now();
+  let resolvedBy = new Date(submitDate);
+  let turnaroundTimeInMs = turnaroundTime*60*60*1000;
+  let timeLeft = turnaroundTimeInMs;
+  let submitDateEndOfWorkingHours = new Date(submitDate);
+  submitDateEndOfWorkingHours.setUTCHours(17);
+  submitDateEndOfWorkingHours.setUTCMinutes(0);
+  submitDateEndOfWorkingHours.setUTCSeconds(0);
+  submitDateEndOfWorkingHours.setUTCMilliseconds(0);
+  let workingHoursLeftToday = submitDateEndOfWorkingHours.getTime() - submitDate.getTime();
+  timeLeft -= workingHoursLeftToday;
+  let remainingFullDays = Math.floor(timeLeft/1000/60/60/8);
+  timeLeft %= 1000*60*60*8;
+
+  for (let i = -1; i < remainingFullDays; i++){
+    resolvedBy.setTime(resolvedBy.getTime() + 1000*60*60*24);
+    if (resolvedBy.getUTCDay() > 5){
+      resolvedBy.setTime(resolvedBy.getTime() + 1000*60*60*24*2);
+    }
+  }
+  resolvedBy.setUTCHours(9);
+  resolvedBy.setUTCMinutes(0);
+  resolvedBy.setUTCSeconds(0);
+  resolvedBy.setUTCMilliseconds(0);
+
+  resolvedBy.setTime(resolvedBy.getTime() + timeLeft);
+
   return resolvedBy;
 }
 
